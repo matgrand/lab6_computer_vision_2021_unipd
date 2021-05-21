759b97b26878b75a7fb5ffc5884f100bf46c2687
matgrand <matteo.grandin@gmail.com>
2021-05-21 20:05:40 +0200

    Something is better than nothing

--- matcher.cpp
+++ matcher.cpp
@@ -1,5 +1,6 @@
 #include "matcher.h"
 
+#define SLOW_MODE false 
 
 using namespace cv;
 using namespace std;
@@ -10,39 +11,39 @@ MyMatcher::MyMatcher(){
 
 void MyMatcher::load_obj(Mat img_of_obj){
 
-  Mat descr;
+	Mat descr;
 	vector<KeyPoint> keypoints;
 
 	Ptr<SIFT> sift = SIFT::create();
 	sift->SIFT::detect(img_of_obj, keypoints);   //detect keypoints
 	sift->SIFT::compute(img_of_obj, keypoints, descr);  //compute descriptor
-  obj_imgs.push_back(img_of_obj);
-  obj_descr.push_back(descr);
-  obj_keypts.push_back(keypoints);
+	obj_imgs.push_back(img_of_obj);
+	obj_descr.push_back(descr);
+	obj_keypts.push_back(keypoints);
 
 }
 
 vector<vector<Point2f>> MyMatcher::match(Mat frame){
 
-  Mat descr;
-  vector<KeyPoint> keypoints;
+	Mat descr;
+	vector<KeyPoint> keypoints;
 
-  Ptr<SIFT> sift = SIFT::create();
-  sift->SIFT::detect(frame, keypoints);   //detect keypoints
-  sift->SIFT::compute(frame, keypoints, descr);  //compute descriptor
+	Ptr<SIFT> sift = SIFT::create();
+	sift->SIFT::detect(frame, keypoints);   //detect keypoints
+	sift->SIFT::compute(frame, keypoints, descr);  //compute descriptor
 
-  Ptr<BFMatcher> matcher = BFMatcher::create(cv::NORM_L2);   //create a matcher of type BFMatcher to match descriptors
+	Ptr<BFMatcher> matcher = BFMatcher::create(cv::NORM_L2);   //create a matcher of type BFMatcher to match descriptors
 
-  vector<vector<DMatch>> matches;  //one vector of metches for each example
+	vector<vector<DMatch>> matches;  //one vector of metches for each example
 
 
-  for(int i = 0; i < obj_imgs.size(); ++i){
-    //for loop to match the frame with the example images
+	for(int i = 0; i < obj_imgs.size(); ++i){
+		//for loop to match the frame with the example images
 
-    vector<DMatch> v;
-    matcher->BFMatcher::match(obj_descr[i], descr, v);
-    matches.push_back(v);
-  }
+		vector<DMatch> v;
+		matcher->BFMatcher::match(obj_descr[i], descr, v);
+		matches.push_back(v);
+	}
 
   //thresholding to pick only good matches, for every example compute the minimum match distance.
   //Then accept as good match if its distance is below min_distance times threshold
@@ -69,44 +70,66 @@ vector<vector<Point2f>> MyMatcher::match(Mat frame){
 				good_matches[i].push_back(matches[i][j]);
 			}
 		}
-    std::cout << "Number of good matches: " << good_matches[i].size() << '\n';
+		std::cout << "Number of good matches: " << good_matches[i].size() << '\n';
 	}
 
-  vector<vector<Point2f>> obj(4);
-  vector<vector<Point2f>> scene(4);
-  vector<Point2f> obj_corners(4);
-  vector<Point2f> scene_corners;
+	vector<vector<Point2f>> obj;
+	vector<vector<Point2f>> scene;
+	vector<Point2f> obj_corners(4);
+	vector<Point2f> scene_corners(4);
 
-  for(int i = 0; i < obj_imgs.size(); ++i){
+	for(int i = 0; i < obj_imgs.size(); ++i){
 
-    obj_corners[0] = Point2f(0, 0);
-    obj_corners[1] = Point2f( (float)obj_imgs[i].cols, 0 );
-    obj_corners[2] = Point2f( (float)obj_imgs[i].cols, (float)obj_imgs[i].rows );
-    obj_corners[3] = Point2f( 0, (float)obj_imgs[i].rows );
+		obj_corners[0] = Point2f(0, 0);
+		obj_corners[1] = Point2f( (float)obj_imgs[i].cols, 0 );
+		obj_corners[2] = Point2f( (float)obj_imgs[i].cols, (float)obj_imgs[i].rows );
+		obj_corners[3] = Point2f( 0, (float)obj_imgs[i].rows );
 
+		cout << "corners " << i << " = " << obj_corners[0] << " " << obj_corners[1];
+		cout << " " << obj_corners[2] << " " << obj_corners[3] << endl;
+		vector<Point2f> obj_to_add;
+		vector<Point2f> scene_to_add;
+		for(int j = 0; j < good_matches[i].size(); ++j){
+			//-- Get the keypoints from the good matches
+			obj_to_add.push_back(obj_keypts[i][good_matches[i][j].queryIdx].pt);
+			scene_to_add.push_back(keypoints[good_matches[i][j].trainIdx].pt);
+		}
+		obj.push_back(obj_to_add);
+		//add the projected points
+		scene.push_back(scene_to_add);
 
-    for(int j = 0; j < good_matches[i].size(); ++j){
-    //-- Get the keypoints from the good matches
-      obj[i].push_back(obj_keypts[i][good_matches[i][j].queryIdx].pt);
-      scene[i].push_back(keypoints[good_matches[i][j].trainIdx].pt);
-    }
+		//find the projected corners
+		Mat H = findHomography(obj[i], scene[i], RANSAC);
+		perspectiveTransform( obj_corners, scene_corners, H);
+    
+		//add the corners at the end of the vector
+		for (int k = 0; k < 4; k++) {
+			Point2f corner = scene_corners[k];
+			scene[i].push_back(corner);
+		}
+	
+		if (SLOW_MODE) {
+			cv::Mat out;
+			cv::drawMatches(obj_imgs[i], obj_keypts[i], frame, keypoints, good_matches[i], out);
+			cv::imshow("Matches", out);
+			waitKey(0);
+		}
+	}
 
-    Mat H = findHomography(obj[i], scene[i], RANSAC);
-    perspectiveTransform( obj_corners, scene_corners, H);
-    scene.push_back(scene_corners);   //questa linea se metto scene[i] da errore. In questo modo però hai prima i vettori dei matches
-                                      //e poi altri quattro vettori con solo i corner
+  
+	std::cout << "match() ret size = " <<scene.size() << '\n';
+	cout << "match() ret[0] size = " << scene[0].size() << endl;
+	cout << "match() ret[last] size = " << scene[scene.size()-1].size() << endl;
 
-  }
-  /*
-  cv::Mat out;
-	cv::drawMatches(obj_imgs[0], obj_keypts[0], frame, keypoints, good_matches[0], out);
-	cv::imshow("Matches", out);
-  vector<Point2f> obj_corners(4);
-  vector<Point2f> scene_corners(4);
-  */
-  std::cout << scene.size() << '\n';
+	cout << "corners 0:" << endl;
+	cout << scene[0][scene[0].size() - 1] << " " << scene[0][scene[0].size() - 2];
+	cout << scene[0][scene[0].size() - 3] << " " << scene[0][scene[0].size() - 4] << endl;
 
-  //vector<vector<Point2f>> obj;
-  return scene;
+	//cv::Mat out;
+	//cv::drawMatches(obj_imgs[0], obj_keypts[0], frame, keypoints, good_matches[0], out);
+	//cv::imshow("Matches", out);
+	//waitKey(0);
+
+	return scene;
 
 }
--- tracker.cpp
+++ tracker.cpp
@@ -1,9 +1,9 @@
 #include "tracker.h"
 
 #define MAX_KEPT 10
-#define SLOW_MODE true
+#define SLOW_MODE false
 
-const int pyramid_levels = 2;
+const int pyramid_levels = 3;
 
 template <class T>
 void remove_first(vector<T> v) {
@@ -12,18 +12,38 @@ void remove_first(vector<T> v) {
 
 MyTracker::MyTracker(vector<vector<Point2f>> to_track, Mat initial_frame) {
 	
-	//separate corners from features
-	vector<vector<Point2f>> features_to_track = to_track; //change this
-	vector<vector<Point2f>> rect_corners = to_track; //change this
-	
+	cout << endl << "TRACKER" << endl << endl;
+
 	num_objs = to_track.size(); //number of objects to track
-	
+
+	vector<vector<Point2f>> features_to_track, rect_corners;
+
+	//separate corners from features
+	for (int obj_i = 0; obj_i < num_objs; obj_i++) {
+		vector<Point2f> i_th_vec = to_track[obj_i];
+		//extract rect points
+		vector<Point2f> rect_to_add(&i_th_vec[i_th_vec.size()-4], &i_th_vec[i_th_vec.size()]);
+		//add rect points
+		rect_corners.push_back(rect_to_add);
+		//extract features
+		vector<Point2f> feat_to_add(&i_th_vec[0], &i_th_vec[i_th_vec.size()-4]);
+		//add features
+		features_to_track.push_back(feat_to_add);
+	}
+
 	// adds MAX_KEPT to the things, to create the "previous MAX_KEPT things"
 	for (int frame_i = 0; frame_i < MAX_KEPT; frame_i++) {
 		recent_frames.push_back(initial_frame); 
 		objs_points.push_back(features_to_track);
-		H_vec.push_back(findHomography(features_to_track, features_to_track, RANSAC));
 		rect_points.push_back(rect_corners);
+
+		vector<Mat> H_to_add;
+		for (int obj_i = 0; obj_i < num_objs; obj_i++) {
+			Mat H = findHomography(features_to_track[obj_i], features_to_track[obj_i], RANSAC);
+			H_to_add.push_back(H);
+		}
+		H_vec.push_back(H_to_add);
+
 	}
 
 	//assign a random color to each obj
@@ -34,6 +54,13 @@ MyTracker::MyTracker(vector<vector<Point2f>> to_track, Mat initial_frame) {
 		int b = rng.uniform(0, 256);
 		colors.push_back(Scalar(r, g, b));
 	}
+
+	if (SLOW_MODE) {
+		cout << "num_objs= " << num_objs << endl;
+		//imshow("Initial Frame", recent_frames.back());
+		waitKey(0);
+	}
+
 }
 
 void MyTracker::track(Mat frame) {
@@ -77,20 +104,21 @@ void MyTracker::track(Mat frame) {
 			if (status[i] == 1) {
 				p_new_good.push_back(p_new[i]);
 				// draw the tracks
-				line(mask, p_new[i], p_old[i], colors[i], 2);
-				circle(frame, p_new[i], 5, colors[i], -1);
+				//line(mask, p_new[i], p_old[i], colors[i], 2);
+				//circle(frame, p_new[i], 5, colors[i], -1);
 			}
 		}
+
+		cout << "good points remeaining " << obj_i << " = " << p_new_good.size() << endl;
 
 		//add the new good point to the vector of points
 		points_to_add.push_back(p_new_good);
 
 		if (SLOW_MODE) {
-			Mat img_out; 
-			add(frame, mask, img_out);
-			imshow("Frame", img_out);
+			//Mat img_out; 
+			//add(frame, mask, img_out);
+			//imshow("Frame", img_out);
 		}
-
 
 		//ESTIMATE TRANSLATION 
 		H_to_add.push_back(findHomography(p_old, p_new_good, RANSAC));
@@ -107,17 +135,28 @@ void MyTracker::track(Mat frame) {
 
 }
 
-
 void MyTracker::draw_rect() {
 	vector<vector<Point2f>> rect_to_add;
+	Mat out = recent_frames.back();
 	for (int obj_i = 0; obj_i < num_objs; obj_i++) {
 		vector<Point2f> curr_rect, new_rect;
 		curr_rect = rect_points.back()[obj_i];
 		//translate the rect
-		perspectiveTransform(curr_rect, new_rect, H_vec.back());
+		perspectiveTransform(curr_rect, new_rect, H_vec[obj_i].back());
 		//add the rect
 		rect_to_add.push_back(new_rect);
+
+
+		//draw
+		line(out, new_rect[0], new_rect[1], colors[obj_i], 8);
+		line(out, new_rect[1], new_rect[2], colors[obj_i], 8);
+		line(out, new_rect[2], new_rect[3], colors[obj_i], 8);
+		line(out, new_rect[3], new_rect[0], colors[obj_i], 8);
 	}
+
+	imshow("Tracking", out);
+	waitKey(1);
+
 	//add the rects
 	rect_points.push_back(rect_to_add);
 	remove_first(rect_points);
