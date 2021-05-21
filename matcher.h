a10b246b0b686296f88c3a246e0403d395e363d5
matgrand <matteo.grandin@gmail.com>
2021-05-21 09:42:44 +0200

    added a link of a tutorial for the matching part

--- matcher.h
+++ matcher.h
@@ -18,7 +18,8 @@ public:
 	// then adds the object to the list of objects to match
 	void load_obj(Mat img_of_obj);
 
-	// match: return a vector of vectors of points : positions of the good matches, one vector for each obj 
+	// match: return a vector of vectors of points : positions of the good matches, one vector for each obj
+	// See https://docs.opencv.org/3.4/d7/dff/tutorial_feature_homography.html
 	vector<vector<Point2f>> match(Mat frame);
 
 
